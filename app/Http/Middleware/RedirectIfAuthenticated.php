6f2b01a5b69ac4352cbc0fd4895db87002184fd6
Stetson Lewis <stetson.lewis@outlook.com>
2021-04-25 21:54:05 -0400

    remove redirect from middleware, return message

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,7 +24,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return new JsonResponse(['message' => 'already authenticated'], 418);
             }
         }
 
