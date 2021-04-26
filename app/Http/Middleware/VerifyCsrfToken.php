ca55c9a47c4bcac514ceab64f1175d511ef621f0
Stetson Lewis <stetson.lewis@outlook.com>
2021-04-25 21:53:40 -0400

    update config to work as API add sample routes and user update cors and session config add fix for csrf token to work when initiating requests from rest client

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Cookie\CookieValuePrefix;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -14,4 +16,37 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+        /**
+     * Get the CSRF token from the request.
+     *
+     * Overrides Laravel's implementation because REST clients cannot handle
+     * extra padded characters at the end of the cookie/token
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function getTokenFromRequest($request) : ?string
+    {
+        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
+
+        if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
+            try {
+                // new block
+                $header = base64_decode($header);
+
+                $pos = strpos($header, '}');
+                $header = substr($header, 0, $pos+1);
+
+                $header = base64_encode($header);
+                /**/
+
+                $token = CookieValuePrefix::remove($this->encrypter->decrypt($header, static::serialized()));
+            } catch (DecryptException $e) {
+                $token = '';
+            }
+        }
+
+        return $token;
+    }
 }
